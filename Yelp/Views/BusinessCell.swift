5cfd5cb41e8458eeafd6662afaa344ce51bfbc9c
user142219 <user142219@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-20 20:47:42 -0400

    First commit for A3

---
+++ Yelp/Views/BusinessCell.swift
@@ -0,0 +1,52 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by user142219 on 9/18/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnview: UIImageView!
+    
+    @IBOutlet weak var namelabel: UILabel!
+    
+    @IBOutlet weak var distancelabel: UILabel!
+    
+    @IBOutlet weak var reviewscountlabel: UILabel!
+    
+    @IBOutlet weak var ratingimageview: UIImageView!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var catalabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            namelabel.text = business.name
+            thumbnview.setImageWith(business.imageURL!)
+            catalabel.text = business.categories
+            addressLabel.text = business.address
+            reviewscountlabel.text = "\(business.reviewCount!) Reviews"
+            ratingimageview.image = business.ratingImage
+            distancelabel.text = business.distance
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbnview.layer.cornerRadius = 3
+        thumbnview.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
